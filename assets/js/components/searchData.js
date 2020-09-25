fdab0dd21ca0fffc171596f1f907229c7952b7da
Mads Haerup <506094@rts-365.dk>
2020-09-25 11:59:29 +0200

    change

--- assets/js/components/searchData.js
+++ assets/js/components/searchData.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-  /* $.ajaxSetup({
-     cache: false
-   });*/
+
   //en funktion der kædes sammen med et tryk på en tasturknap
   $('#search').keyup(function() {
     //clearer resultatet
@@ -9,27 +7,29 @@ $(document).ready(function() {
     //får værdien fra søgefeltet og gemmer det i variablen(searchField)
     var searchField = $('#search').val();
     //regular expression som gemmes i en variable
+    //expression modtager 'searchField' som et argument og bruger det til at tjekke søgefeltet 
+    //i er en modifier som bruges til at udføre "case-insensitive matching".
     var expression = new RegExp(searchField, "i");
     //Ajax request for at få JSON data
     $.getJSON('assets/data/searchData.json', function(data) {
-      //resultatet af requesten
-      //data får vi fra JSON
+      //resultatet af requesten er 'data' som fås fra JSON
+      //tjekker data'en vi får fra JSON en efter en via forEach
+      //funktionen retunere key og value fra data
       $.each(data, function(key, value) {
         //søge betingelser
         if (value.name.search(expression) != -1 || value.manufacturer.search(expression) != -1 || value.price.search(expression) != -1) {
-          //Hvad der vises som søgefeltet's resultat
+          //Hvad der vises som søgefeltet's resultat, hvis betingelsen er opfyldt
+          //der bliver tilføjet en li til ul'en, li'en indeholder et image, name, price og manufacturer
           $('#result').append('<li class="list-group-item"><img src="' + value.image + '" height="40" width="40" class="img-thumbnail" /> ' + value.name + ' ' + value.price + ' | <span class="text-muted">' + value.manufacturer + '</span></li>');
         }
       });
     });
   });
-  // trykker på navnet og det åbner i nyt vindue
+  // trykker på søgeresultatet og den åbner produktet
   $('#result').on('click', 'li', function() {
     $("#result").html('');
+    //åbner produktet i et nyt vindue (da vi ikke har en produkt side for hvert produkt)
     window.open('singleProduct__page.html', '_blank');
-    $(window).on('click', function() {
-      $("#result").hide;
-    });
   });
 
 });
